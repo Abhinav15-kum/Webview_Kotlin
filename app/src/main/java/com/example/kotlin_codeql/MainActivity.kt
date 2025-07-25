e204ee2b225d25ca3f7b5be31a81c335415082b6
abhinkumar <abhinav1.kumar@wbd.com>
2025-07-25 14:51:05 +0530

    modify

--- app/src/main/java/com/example/kotlin_codeql/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_codeql/MainActivity.kt
@@ -1,59 +1,44 @@
-package com.example.kotlin_codeql
-
 import android.os.Bundle
+import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.kotlin_codeql.ui.theme.Kotlin_CodeqlTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            var password="XYZQWERT"
 
-            var webView: WebView = WebView(this)
-            setContentView(webView)
+        val webView = WebView(this)
+        setContentView(webView)
 
-            val webSettings: WebSettings = webView.getSettings()
-            webSettings.setJavaScriptEnabled(true)
-            webSettings.setAllowFileAccess(true)
-            webSettings.setAllowContentAccess(true)
-
-            webView.setWebViewClient(WebViewClient())
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
 
 
-            // Load local HTML file from assets
+        webSettings.allowFileAccessFromFileURLs = true
+        webSettings.allowUniversalAccessFromFileURLs = true
 
-            webView.loadUrl("file:///android_asset/index.html")
+
+        webView.addJavascriptInterface(InsecureInterface(), "Android")
+
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
+
+        val url = intent.getStringExtra("url")
+        if (url != null) {
+            webView.loadUrl(url)
         }
-
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Kotlin_CodeqlTheme {
-        Greeting("Android")
+    // Exposes native methods to JS - can be exploited if attacker injects JS
+    class InsecureInterface {
+        @android.webkit.JavascriptInterface
+        fun showToast(message: String) {
+            // Some action
+            println("Message from JS: $message")
+        }
     }
 }
